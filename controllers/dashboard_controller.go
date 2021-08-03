2bc0c1662da504db5dc42ea308c398ad0774586c
Joshua Channon <josh@channon.dev>
2021-08-03 19:40:59 +0800

    Create replicaset for Dashboard

--- controllers/dashboard_controller.go
+++ controllers/dashboard_controller.go
@@ -21,9 +21,11 @@ import (
 	"fmt"
 	"github.com/go-logr/logr"
 	"gopkg.in/yaml.v2"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	kp "kubepoint.io/kubepoint/api/v1alpha1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -40,6 +42,7 @@ type DashboardReconciler struct {
 //+kubebuilder:rbac:groups=kubepoint.io,resources=dashboards/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=kubepoint.io,resources=dashboards/finalizers,verbs=update
 //+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=replicasets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -92,19 +95,88 @@ func (r *DashboardReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 
 	constructDashboardReplicaSet := func(dashboard *kp.Dashboard) (*appsv1.ReplicaSet, error) {
 		name := fmt.Sprintf("%s-%s", "kubepoint", dashboard.Name)
+		replicas := int32(1)
+		labels := map[string]string{
+			"app":       "kubepoint",
+			"dashboard": name,
+		}
 
 		replicaSet := &appsv1.ReplicaSet{
-			ObjectMeta: metav1.ObjectMeta{},
-			Spec:       appsv1.ReplicaSetSpec{},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: dashboard.Namespace,
+			},
+			Spec: appsv1.ReplicaSetSpec{
+				Replicas:        &replicas,
+				MinReadySeconds: 0,
+				Selector: &metav1.LabelSelector{
+					MatchLabels: labels,
+				},
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: labels,
+					},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{{
+							Name:  "homer",
+							Image: "b4bz/homer:21.07.1-arm32v7",
+							Ports: []corev1.ContainerPort{{
+								Name:          "http",
+								ContainerPort: 8080,
+							}},
+							LivenessProbe: &corev1.Probe{
+								Handler: corev1.Handler{
+									HTTPGet: &corev1.HTTPGetAction{
+										Path: "/",
+										Port: intstr.IntOrString{
+											IntVal: 8080,
+										},
+									},
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{{
+								Name:      "config",
+								MountPath: "/www/assets/config.yml",
+								SubPath:   "config.yml",
+								ReadOnly:  true,
+							}},
+						}},
+						Volumes: []corev1.Volume{{
+							Name: "config",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: configMap.Name,
+									},
+								},
+							},
+						}},
+					},
+				},
+			},
 		}
+		if err := ctrl.SetControllerReference(dashboard, replicaSet, r.Scheme); err != nil {
+			return nil, err
+		}
+
+		return replicaSet, nil
+	}
+	replicaSet, err := constructDashboardReplicaSet(&dashboard)
+	if err != nil {
+		log.Error(err, "unable to construct replicaSet from template")
 	}
 
 	if err := r.Create(ctx, configMap); err != nil {
 		log.Error(err, "unable to create config for Dashboard")
 		return ctrl.Result{}, err
 	}
-
 	log.V(1).Info("created configMap for Dashboard")
+
+	if err := r.Create(ctx, replicaSet); err != nil {
+		log.Error(err, "unable to create replicaSet for Dashboard")
+		return ctrl.Result{}, err
+	}
+	log.V(1).Info("created replicaSet for Dashboard")
 
 	return ctrl.Result{}, nil
 }
